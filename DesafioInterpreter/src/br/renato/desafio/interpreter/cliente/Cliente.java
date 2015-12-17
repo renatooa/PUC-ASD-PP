ea185559a41a91e11ba35fd8296c8eeae8ebab4e
Renato Alves <renatoalves.infor@gmail.com>
2015-12-17 13:34:36 -0200

    melhorias

--- DesafioInterpreter/src/br/renato/desafio/interpreter/cliente/Cliente.java
+++ DesafioInterpreter/src/br/renato/desafio/interpreter/cliente/Cliente.java
@@ -6,8 +6,8 @@ public class Cliente {
 
 	public static void main(String[] args) {
 
-		System.err.println(AvaliadorExpression.interpretarCifra("D6/9° C7 Dm7 |"));
-
-
+		System.out.println(AvaliadorExpression.interpretarCifra("D6/9° C7 Dm7"));
+		
+		System.out.println(AvaliadorExpression.interpretarCifra("E6/9° C7 Dm7 Rm"));
 	}
 }
