f8ebba63261984794a94390506326e042167a7aa
Renato <renatoalves.infor@gmail.com>
2015-12-29 00:30:24 -0200

    conexoes remotas

---
+++ TrabalhoLab05x02/src/br/com/trab/cinco/modelo/venda/pagamento/proxy/remoto/AutorizadorCartaoStub.java
@@ -0,0 +1,40 @@
+package br.com.trab.cinco.modelo.venda.pagamento.proxy.remoto;
+
+import br.com.trab.cinco.modelo.venda.pagamento.CartaoInfoDTO;
+import br.com.trab.cinco.modelo.venda.pagamento.PagamentoAutorizavel;
+
+public class AutorizadorCartaoStub implements PagamentoAutorizavel {
+
+	private CartaoInfoDTO cartaoInfo = null;
+	private double aPagar = 0;
+	// private AutorizadorCartaoSkeleton autorizadorCartaoSkeleton
+
+	@Override
+	public boolean verificarPagamento() {
+		// Utiliza o PagamentoAutorizavel Skeleton para verificar externamento o
+		// a autorização para o pagamento com cartão de credito
+		return cartaoInfo != null;
+	}
+
+	public AutorizadorCartaoStub(CartaoInfoDTO cartaoInfo, double aPagar) {
+		super();
+		this.cartaoInfo = cartaoInfo;
+		this.aPagar = aPagar;
+	}
+
+	public CartaoInfoDTO getCartaoInfo() {
+		return cartaoInfo;
+	}
+
+	public void setCartaoInfo(CartaoInfoDTO cartaoInfo) {
+		this.cartaoInfo = cartaoInfo;
+	}
+
+	public double getaPagar() {
+		return aPagar;
+	}
+
+	public void setaPagar(double aPagar) {
+		this.aPagar = aPagar;
+	}
+}
---
+++ TrabalhoLab05x02/src/br/com/trab/cinco/modelo/venda/pagamento/proxy/remoto/AutorizadorChequeStub.java
@@ -0,0 +1,23 @@
+package br.com.trab.cinco.modelo.venda.pagamento.proxy.remoto;
+
+import br.com.trab.cinco.modelo.venda.pagamento.PagamentoAutorizavel;
+
+public class AutorizadorChequeStub implements PagamentoAutorizavel {
+
+	private String carteiraIdentidade = null;
+	private double aPagar = 0;
+	// private AutorizadorChequeSkeleton autorizadorChequeSkeleton
+
+	public AutorizadorChequeStub(String carteiraIdentidade, double aPagar) {
+		super();
+		this.carteiraIdentidade = carteiraIdentidade;
+		this.aPagar = aPagar;
+	}
+
+	@Override
+	public boolean verificarPagamento() {
+		// Utiliza o PagamentoAutorizavel Skeleton para verificar externamento o
+		// a autorização para o cheque
+		return carteiraIdentidade != null;
+	}
+}
